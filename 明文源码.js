a7a731923835ae21d71a3a41f7337187e0ed4511
HKFISHMAN <51708946+HKFISHMAN@users.noreply.github.com>
2025-01-12 18:42:58 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'bcf02d0b-1dad-4497-8300-318fec2f4f06';
 let proxyIP = '';
 //let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
